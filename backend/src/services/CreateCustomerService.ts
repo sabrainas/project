e82918c56dc0a617e1342282fb75ed6e5b54c694
sabrainas <sazonass.m@gmail.com>
2024-02-01 21:46:04 -0300

    gitignore

--- backend/src/services/CreateCustomerService.ts
+++ backend/src/services/CreateCustomerService.ts
@@ -1,27 +1,35 @@
 import prismaClient from "../prisma";
 
-interface CreateCustomerProps{
+interface CreateCustomerProps {
     name: string;
-    email: string
+    email: string;
+    cpf: string;
+    address: string;
+    rg: string
 }
 
-class CreateCustomerService{
-    async execute({name, email}: CreateCustomerProps){
-        
-        if(!name || !email){
+class CreateCustomerService {
+    async execute({ name, email }: CreateCustomerProps) {
+
+        if (!name || !email) {
             throw new Error('name and email are required');
         }
 
-        const customer = await prismaClient.customer.create({
-            data:{
-                name,
-                email,
-                status: true
-            }
-        })
+        try {
+            const customer = await prismaClient.customer.create({
+                data: {
+                    name,
+                    email,
+                    status: true,
+                },
+            });
 
-        return customer
+            return customer;
+        } catch (error) {
+            console.error('Error creating customer:', error);
+            throw new Error('Error creating customer');
+        }
     }
 }
 
-export {CreateCustomerService} 
+export { CreateCustomerService } 
